7921fd4661f8b52bc43a311df2887658b6d5e07c
jasonkqs <jasonkqs@bu.edu>
2019-12-04 11:28:27 -0800

    Add files via upload

---
+++ hw8_2.js
@@ -0,0 +1,20 @@
+"use strict";
+const fileFilter = function(dir, extName) {
+  const fs = require("fs");
+  const path = require("path");
+
+  fs.readdir(dir, function(err, list) {
+    if (err) {
+      throw err;
+    }
+    list.forEach((file, index) => {
+      if (path.extname(file) === "." + extName) {
+        console.log(file);
+      }
+    });
+  });
+};   
+
+
+
+fileFilter("/Users/kqs/Desktop/USTSV/VScode/SampleCode_8", "txt");
